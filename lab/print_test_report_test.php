ed07251eefd93b3ffd3e4e08255b00183bb493ee
hammadshahid625 <hammadshahid625@gmail.com>
2026-05-29 09:48:39 +0500

    print fix

--- lab/print_test_report_test.php
+++ lab/print_test_report_test.php
@@ -21,7 +21,10 @@ if(isset($_GET['token_no']) && $_GET['token_no'] != '')
     $file = $path.".png";
     $ecc = 'L';
     $pixel_Size = 10;
-    $frame_Size = 10;	
+    $frame_Size = 10;
+    if (!is_dir('qr/')) {
+        mkdir('qr/', 0777, true);
+    }
     QRcode::png($token_no, $file, $ecc, $pixel_Size, $frame_Size);    
 }
 function header_data($token_no)
